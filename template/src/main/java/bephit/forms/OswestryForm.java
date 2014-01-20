529d6dbed987882767d8937f2a18fc6cb7196627
deemsys <sangeetha_d@deemsysinc.com>
2014-01-19 23:05:23 -0800

    commited OswestryForm

---
+++ template/src/main/java/bephit/forms/OswestryForm.java
@@ -0,0 +1,19 @@
+package bephit.forms;
+import java.util.List;
+
+import bephit.model.*;
+public class OswestryForm
+{
+	private List<Oswestry> oswestrydetails;
+
+	public List<Oswestry> getOswestrydetails() {
+		return oswestrydetails;
+	}
+
+	public void setOswestrydetails(List<Oswestry> oswestrydetails) {
+		this.oswestrydetails = oswestrydetails;
+	}
+
+	
+	
+}
